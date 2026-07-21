ffd5b2b4933b5b165e7eadd383b127d1c86d8340
shengchun liu <johnnathan@atomicmail.io>
2026-07-21 11:56:01 -0400

    Build static education site

--- build-static-svg.mjs
+++ build-static-svg.mjs
@@ -1,6 +1,6 @@
 import { readFile, writeFile } from "node:fs/promises";
 
-const repo = process.argv[2] || "AetherGamesOfficial/AetherStatic";
+const repo = process.argv[2] || "AetherGamesOfficial/AetherEducationStatic";
 const cdnBase = `https://cdn.jsdelivr.net/gh/${repo}@main/`;
 const buildStamp = "favicon-refresh-20260721";
 const pages = ["studyhub", "enrichment", "resources", "research", "settings", "loading"];
